0d5b269ca6dd28f45894c64611e75613c5174b28
Mittapalli Praneeth <sunnypraneeth3119@gmail.com>
2025-03-29 22:10:57 +0530

    CourseSelling

---
+++ CourseSellingWebsite/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const userroutes = require("./user_auth");
+const adminroutes=require("./admin_auth");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser())
+const cors=require("cors")
+app.use(express.json()); 
+app.use(cors({
+    origin: ["http://localhost:3000" ,"http://127.0.0.1:5500", "http://localhost:5500"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true 
+}));
+
+app.use("/user", userroutes);
+app.use("/admin",adminroutes)
+
+app.get("/logout", (req, res) => {
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: false, 
+    });
+    return res.json({ msg: "You have successfully logged out." });
+});
+
+app.listen(3001, () => console.log("Server running on port 3001"));
+
+// const express = require("express");
+// const app = express();
+// const userroutes = require("./user_auth");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+// app.use(cors({
+//     origin: "http://127.0.0.1:5500",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true
+// }));
+
+// app.use("/user", userroutes);
+
+// app.listen(3001, () => console.log("Server running on port 3001"));
+
