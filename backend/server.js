d9cd6913cd5ebde119c95d1b450242584ba5fcef
Mittapalli Praneeth <sunnypraneeth3119@gmail.com>
2025-03-31 09:20:36 +0530

    "fourth"

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get("/logout", (req, res) => {
 });
 
 console.log(process.env.PORT);
-
+const port=process.env.PORT || 3000
 
 
 app.listen(process.env.PORT, () => console.log("Server running on port "+process.env.PORT));
